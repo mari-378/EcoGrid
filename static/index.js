ed6be6675d467c32eb423333737fbda14c876392
Leticya <leticyammedeiros@gmail.com>
2025-12-03 19:41:06 -0300

    refactor: remove chamada da antiga seção de simulação no js

--- static/index.js
+++ static/index.js
@@ -1,10 +1,8 @@
 import { createSVG, buildHierarchy, buildTree } from "./js/createTree.js";
-import { setupSimulation } from "./js/simulateEvent.js";
 import { setupChangeNode } from "./js/changeNode.js";
 
 const baseUrl = document.body.getAttribute("data-base-url") || "";
 const btnLoadTree = document.getElementById("btn-load-tree");
-const simulationForm = document.querySelector("#simulation form");
 const changeForm = document.querySelector("#change-node form");
 
 btnLoadTree.addEventListener("click", (e) => {
@@ -30,12 +28,6 @@ btnLoadTree.addEventListener("click", (e) => {
     })
     .catch((err) => console.error("Erro ao carregar a árvore:", err));
 });
-
-if (simulationForm) {
-  setupSimulation(simulationForm);
-} else {
-  console.error("Formulário de simulação não encontrado.");
-}
 
 if (changeForm) {
   setupChangeNode(changeForm, baseUrl);
